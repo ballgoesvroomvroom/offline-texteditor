b03dee5ae6b4cb7a90cbbcbad82bc57e86b37a1f
whiteball <54146032+ballgoesvroomvroom@users.noreply.github.com>
2021-12-08 14:50:29 +0000

    add 1

--- src/app.js
+++ src/app.js
@@ -1,45 +1,86 @@
 $(document).ready(function() {
-	var currentopenedid = "";
-	var openedtabscount = 0;
+	var currentpreferencesopenedid = "";
+	var openedtabscount = $(".tabpages").length;
 
-	function loadnewpage(filename, filecontents, toshow = false) {
+	/* store all contents */
+	var currentopentabid = ""
+	function storeeditorcontents(contents)
+	function loadeditor(contents) {
+		$("#editoreditable").html(contents);
+	}
+	function loadnewpage(filename, filecontents = "", toshow = false) {
 		openedtabscount += 1;
 
-		jQuery('<div>', {
-			id: "tabpage-".concat(openedtabscount),
-			class: 'tabpages',
-		}).appendTo('#editortopbar');
+		var newid_wo_hash = "tabpage-".concat(openedtabscount);
+		var newid = "#".concat(newid_wo_hash)
+		/* main container; constructing new html */
+		jQuery("<div>", {
+			id: newid_wo_hash,
+			"class": "tabpages",
+		}).appendTo("#editortopbar");
+		jQuery("<p>", {
+			"class": "tabpages-title",
+		}).appendTo(newid).html(filename);
+		jQuery("<p>", {
+			"class": "tabpages-closebutton",
+		}).appendTo(newid).html("x");
+
+		if (toshow) {
+			loadeditor(filecontents);
+		}
+	}
+
+	function createnewpage() {
+		loadnewpage("untitled", "", true)
 	}
 
 	$(".menutext").click(function() {
 		var targetoptions = "#".concat(event.target.id.concat("options"));
 
 		if ($(targetoptions).css("display") == "none") {
-			if (currentopenedid != "" && targetoptions != currentopenedid) {
+			if (currentpreferencesopenedid != "" && targetoptions != currentpreferencesopenedid) {
 				/* close the current opened one*/
-				$(currentopenedid).css("display", "none");
+				$(currentpreferencesopenedid).css("display", "none");
 			}
 			$(targetoptions).css("display", "flex");
 
-			currentopenedid = targetoptions;
+			currentpreferencesopenedid = targetoptions;
 		} else {
 			$(targetoptions).css("display", "none");
-			currentopenedid = "";
+			currentpreferencesopenedid = "";
 		}
 	});
 
-	/*$(document).bind('keydown', 'ctrl+n', function(e) {
-		e.preventDefault();
-		console.log("HELLO ".concat(e));
-	});*/
-	$("#editoreditable").on("keyup", function(e) {
-		console.log(e.keyCode)
+	/*key binds*/
+	var IS_SHIFT_DOWN = false;
+	var IS_CTRL_DOWN  = false;
+	var IS_ALT_DOWN   = false;
+	$(document).on("keyup", function(e) {
 		//detect 'tab' key
 		if (e.keyCode == 9) {
 			//add tab
 			document.execCommand("insertHTML", false, "&#009");
 			//prevent focusing on next element
 			e.preventDefault();
+		} else if (e.keyCode == 88 && IS_CTRL_DOWN == true) {
+			e.preventDefault();
+
+			createnewpage();
+		} else if (e.keyCode == 16) {
+			IS_SHIFT_DOWN = false;
+		} else if (e.keyCode == 17) {
+			IS_CTRL_DOWN = false;
+		} else if (e.keyCode == 18) {
+			IS_ALT_DOWN = false;
+		}
+	});
+	$(document).on("keydown", function(e) {
+		if (e.keyCode == 16) {
+			IS_SHIFT_DOWN = true;
+		} else if (e.keyCode == 17) {
+			IS_CTRL_DOWN = true;
+		} else if (e.keyCode == 18) {
+			IS_ALT_DOWN = true;
 		}
 	});
 	$("#editoreditable").on("paste", function(e) {
@@ -57,6 +98,4 @@ $(document).ready(function() {
 			document.execCommand("paste", false, text);
 		}
 	});
-
-
 });
