b131b9e00fc31b245b675a00e097b049de74f917
whiteball <chongjayden07@gmail.com>
2021-12-08 21:06:34 +0800

    add 3

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 	var currentopenedid = "";
+	var openedtabscount = 0;
+
+	function loadnewpage(filename, filecontents, toshow = false) {
+		openedtabscount += 1;
+
+		jQuery('<div>', {
+			id: "tabpage-".concat(openedtabscount),
+			class: 'tabpages',
+		}).appendTo('#editortopbar');
+	}
 
 	$(".menutext").click(function() {
 		var targetoptions = "#".concat(event.target.id.concat("options"));
@@ -18,8 +28,35 @@ $(document).ready(function() {
 		}
 	});
 
-	$(document).bind('keydown', 'ctrl+n', function(e) {
+	/*$(document).bind('keydown', 'ctrl+n', function(e) {
 		e.preventDefault();
 		console.log("HELLO ".concat(e));
+	});*/
+	$("#editoreditable").on("keyup", function(e) {
+		console.log(e.keyCode)
+		//detect 'tab' key
+		if (e.keyCode == 9) {
+			//add tab
+			document.execCommand("insertHTML", false, "&#009");
+			//prevent focusing on next element
+			e.preventDefault();
+		}
 	});
+	$("#editoreditable").on("paste", function(e) {
+		console.log("BYE")
+		e.preventDefault();
+		var text = '';
+		if (e.clipboardData || e.originalEvent.clipboardData) {
+			text = (e.originalEvent || e).clipboardData.getData("text/plain");
+		} else if (window.clipboardData) {
+			text = window.clipboardData.getData("Text");
+		}
+		if (document.queryCommandSupported("insertText")) {
+			document.execCommand("insertText", false, text);
+		} else {
+			document.execCommand("paste", false, text);
+		}
+	});
+
+
 });
